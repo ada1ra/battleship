bd5cf3f88327804e205155811497db3a436737ea
ada1ra <adada0901@gmail.com>
2026-04-11 02:33:08 +0300

    Add dummy test for CI testing

---
+++ src/test/kotlin/DummyTest.kt
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+class DummyTest {
+    @Test
+    fun `dummy test always passes`() {
+        assertTrue(true)
+    }
+}
